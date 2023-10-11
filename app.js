a440bcd4012bc1de974be86466010dabcbef7614
dhruvisonani123 <dhruvisonaniid@gmail.com>
2023-10-11 10:29:03 +0530

    commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var empbankdetail = require("./routes/empbankdetail.js");
 var notes = require("./routes/notes.js");
+var leave=require("./routes/leave.js")
 
 // var punching=require("./router/punching.js");
 var PunchinRouter = require("./routes/punching.js");
@@ -39,7 +40,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/empbankdetail", empbankdetail);
 app.use("/notes", notes);
-
+app.use("/leave",leave)
 app.use("/punching", PunchinRouter);
 app.use("/chats", ChatRouter);
 app.use("/holiday", HolidaysRouter);
---
+++ routes/leave.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const RequestLeave = require("../models/leave");
+
+// Create a new leave request
+router.post("/requestleave", async (req, res) => {
+  try {
+    // Extract the leave request data from the request body
+    const { fromdate, todate, leavetype, reasonofleave } = req.body;
+
+    // Create a new leave request using the RequestLeave model
+    const newLeaveRequest = new RequestLeave({
+      fromdate,
+      todate,
+      leavetype,
+      reasonofleave,
+    });
+
+    // Save the new leave request to the database
+    await newLeaveRequest.save();
+
+    res.status(201).json({
+      statusCode: 201,
+      message: "Leave request created successfully",
+      leaveRequest: newLeaveRequest,
+    });
+  } catch (error) {
+    // Handle any errors that occur during the process
+    res.status(500).json({
+      statusCode: 500,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+});
+
+
+
+
+module.exports = router;

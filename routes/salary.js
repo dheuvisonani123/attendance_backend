40cd2dc29b9ebc9c5e778200cdf000fea78442a2
dhruvisonani123 <dhruvisonaniid@gmail.com>
2023-12-18 15:49:05 +0530

    commit

--- routes/salary.js
+++ routes/salary.js
@@ -25,4 +25,22 @@ router.post("/salary", async (req, res) => {
 
 
 
+  router.get("/salary",async (req, res) => {
+    try{
+      var data=await salary.find();
+
+      res.json({
+        statusCode:200,
+        data:data,
+        message:"retrive succesfully",
+      });
+    }
+    catch(error){
+      res.json({
+        statusCode:500,
+        message:error.message,
+      });
+    }   
+  });
+
 module.exports = router;
